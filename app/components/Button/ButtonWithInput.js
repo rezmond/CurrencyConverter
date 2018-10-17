d59bac6284bf5b725db2d3141e1d8814cc538532
rezmond <rezx10m@gmail.com>
2018-10-17 10:24:47 +0300

    lesson 2

---
+++ app/components/Button/ButtonWithInput.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+function ButtonWithInput () {
+  return (
+    <View />
+  );
+}
---
+++ app/components/Button/styles.js
@@ -0,0 +1,9 @@
+/* author: makarov */
+
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+const styles = EStyleSheet.create({
+
+});
+
+export default styles;
